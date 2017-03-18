afc158a02bea6a7178c6b84394b114cc231a7b74
Julio Fernandez <jfernandez74@gmail.com>
2017-03-18 23:05:44 +0100

    Fixed class route and namespace

--- app/Providers/ObjectItemServiceProvider.php
+++ app/Providers/ObjectItemServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Repository\Contract\ObjectItemRepositoryInterface;
 use App\Repository\ObjectItemRepository;
+use App\Services\Contract\ObjectItemServiceInterface;
 use App\Services\ObjectItemService;
 use Illuminate\Support\ServiceProvider;
 
@@ -11,7 +12,7 @@ class ObjectItemServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind(ObjectItemServiceProvider::class, ObjectItemService::class);
+        $this->app->bind(ObjectItemServiceInterface::class, ObjectItemService::class);
         $this->app->bind(ObjectItemRepositoryInterface::class, ObjectItemRepository::class);
     }
 }
