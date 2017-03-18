8c19c65e067d63e6a0112b51a3b9237123a50329
Julio Fernandez <jfernandez74@gmail.com>
2017-03-18 21:31:57 +0100

    Added validation and API message translations

---
+++ resources/lang/en/api.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'client' => [
+        'messages'   => [
+            'http_ok'                    => 'Ok',
+            'http_not_found'             => 'Not found',
+            'http_created'               => 'Object successfully created',
+            'http_patched'               => 'Object successfully updated',
+            'http_destroyed'             => 'Object successfully deleted',
+            'http_internal_server_error' => 'Internal server error',
+            'http_unprocessable_entity'  => 'Model validation error occurred',
+            'http_forbidden'             => 'Access denied',
+            'http_destroy_error'         => 'Can not delete entity',
+        ],
+    ],
+];
