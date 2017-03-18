9e7be4a18f98c9b9778aed361dcae5891ee965d5
Julio Fernandez <jfernandez74@gmail.com>
2017-03-18 19:50:08 +0100

    Created base repository classes

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Repository;
+
+use App\Models\User;
+use App\Repository\Contract\BaseRepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+abstract class BaseRepository implements BaseRepositoryInterface
+{
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * @var User
+     */
+    protected $user;
+
+    public function __construct()
+    {
+        $this->user = Auth::check() ? Auth::user() : Auth::guard('api')->user();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function findAll()
+    {
+        return $this->getModel()->get();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function create(array $attributes)
+    {
+        return $this->getModel()->create($attributes);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function update(Model $entity, array $attributes)
+    {
+        $entity->fill($attributes);
+        $entity->save();
+
+        return $entity;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function delete(Model $entity)
+    {
+        $entity->delete();
+
+        return $entity;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getUser()
+    {
+        return Auth::check() ? Auth::user() : Auth::guard('api')->user();
+    }
+}
---
+++ app/Repositories/Contracts/BaseRepositoryInterface.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repository\Contract;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+interface BaseRepositoryInterface
+{
+    /**
+     * @return Collection
+     */
+    public function findAll();
+
+    /**
+     * @param array $attributes
+     * @return Model
+     */
+    public function create(array $attributes);
+
+    /**
+     * @param Model $entity
+     * @param array $attributes
+     * @return Model
+     */
+    public function update(Model $entity, array $attributes);
+
+    /**
+     * @param Model $entity
+     * @return boolean
+     */
+    public function delete(Model $entity);
+
+    /**
+     * @return Model
+     */
+    public function getModel();
+
+    /**
+     * @return User
+     */
+    public function getUser();
+}
