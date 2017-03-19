a46bcb8e529f0b1b87fe71dbbe398ecec2c20d6d
Julio Fernandez <jfernandez74@gmail.com>
2017-03-19 14:51:42 +0100

    Auth controllers and routes

---
+++ app/Policies/ObjectItemPolicy.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\ObjectItem;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ObjectItemPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the objectItem.
+     *
+     * @param  User $user
+     * @param  ObjectItem $objectItem
+     * @return mixed
+     */
+    public function view(User $user, ObjectItem $objectItem)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create objectItems.
+     *
+     * @param User $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the objectItem.
+     *
+     * @param  User $user
+     * @param  ObjectItem $objectItem
+     * @return mixed
+     */
+    public function update(User $user, ObjectItem $objectItem)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the objectItem.
+     *
+     * @param  User $user
+     * @param  ObjectItem $objectItem
+     * @return mixed
+     */
+    public function delete(User $user, ObjectItem $objectItem)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 Route::group(['namespace' => 'Web'], function () {
     Route::get('/', 'ObjectItemWebController@index');
+    Route::get('/home', 'ObjectItemWebController@index');
     Route::get('/objects', 'ObjectItemWebController@index');
+    Route::post('/objects/search', 'ObjectItemWebController@search');
+    Route::get('/objects/create', 'ObjectItemWebController@create');
     Route::get('/objects/{id}', 'ObjectItemWebController@show');
-    Route::post('/objects/create', 'ObjectItemWebController@create');
     Route::post('/objects', 'ObjectItemWebController@store');
     Route::get('/objects/{id}/edit', 'ObjectItemWebController@edit');
     Route::patch('/objects/{id}', 'ObjectItemWebController@update');
@@ -11,4 +13,6 @@ Route::group(['namespace' => 'Web'], function () {
     Route::patch('/objects/{parent}/attach/{child}', 'ObjectItemWebController@attachObjectItem');
     Route::patch('/objects/{parent}/detach/{child}', 'ObjectItemWebController@detachObjectItem');
 });
+
+Auth::routes();
 
