21d16bcdcece7cc22d58db33ce459c57c42a02ad
Julio Fernandez <jfernandez74@gmail.com>
2017-03-19 19:54:56 +0100

    Added secure route

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ Route::group(['namespace' => 'Web'], function () {
     Route::get('/home', 'ObjectItemWebController@index');
     Route::get('/objects', 'ObjectItemWebController@index');
     Route::post('/objects/search', 'ObjectItemWebController@search');
-    Route::get('/objects/create', 'ObjectItemWebController@create');
+    Route::get('/objects/create', 'ObjectItemWebController@create')->middleware('can:create,App\Models\ObjectItem');
     Route::get('/objects/{id}', 'ObjectItemWebController@show');
-    Route::post('/objects', 'ObjectItemWebController@store');
-    Route::get('/objects/{id}/edit', 'ObjectItemWebController@edit');
-    Route::patch('/objects/{id}', 'ObjectItemWebController@update');
-    Route::delete('/objects/{id}', 'ObjectItemWebController@destroy');
+    Route::post('/objects', 'ObjectItemWebController@store')->middleware('can:create,App\Models\ObjectItem');
+    Route::get('/objects/{id}/edit', 'ObjectItemWebController@edit')->middleware('can:update,App\Models\ObjectItem');
+    Route::patch('/objects/{id}', 'ObjectItemWebController@update')->middleware('can:update,App\Models\ObjectItem');
+    Route::delete('/objects/{id}', 'ObjectItemWebController@destroy')->middleware('can:delete,App\Models\ObjectItem');
     Route::patch('/objects/{parent}/attach/{child}', 'ObjectItemWebController@attachObjectItem');
     Route::patch('/objects/{parent}/detach/{child}', 'ObjectItemWebController@detachObjectItem');
 });
