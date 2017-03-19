1f23ae1a697a4f3aa6973eedf35dd12924cbf2f1
Julio Fernandez <jfernandez74@gmail.com>
2017-03-19 01:07:38 +0100

    Helper test trait

---
+++ tests/Api/ObjectItemApiTestTrait.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Api;
+
+use App\Models\ObjectItem;
+use DB;
+use Faker\Factory as Faker;
+
+trait ObjectItemApiTestTrait
+{
+    /**
+     * @param array $attributes
+     * @return ObjectItem
+     */
+    public function makeObjectItem($attributes = [])
+    {
+        $data = $this->fakeData($attributes);
+
+        /** @var ObjectItem $team */
+        $objectItem_id = DB::table('object_items')->insertGetId($data);
+
+        $objectItem = ObjectItem::find($objectItem_id);
+
+        return $objectItem;
+    }
+
+    /**
+     * @param $parent_id
+     * @param $child_id
+     * @return bool
+     */
+    public function attachObject($parent_id, $child_id)
+    {
+        return DB::table('object_items_relations')->insert(
+            [
+                'parent_id' => $parent_id,
+                'child_id'  => $child_id,
+            ]
+        );
+    }
+
+    /**
+     * @param array $attributes
+     * @return array
+     */
+    public function fakeData($attributes = [])
+    {
+        $fake = Faker::create();
+
+        return array_merge([
+            'name'        => $fake->sentence(rand(2, 3)),
+            'description' => $fake->paragraph,
+            'type'        => $fake->randomElement(['user', 'pc', 'tablet', 'server', 'service', 'website']),
+        ], $attributes);
+    }
+}
